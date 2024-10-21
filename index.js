d98f87ffa000d3d817ff096065968c010c146046
jvseds <jvaaltonen@gmail.com>
2024-10-21 11:38:59 +0300

    added fetchPosts async function; still need to correctly style

--- index.js
+++ index.js
@@ -25,3 +25,26 @@ async function fetchUsers() {
 }
 
 fetchUsers();
+
+// fetch posts for each user
+async function fetchPosts(userID) {
+  try {
+    let posts = await fetch("https://jsonplaceholder.typicode.com/posts");
+    let postsData = await posts.json();
+
+    // create div to hold user's posts
+    postsData.forEach((post) => {
+      if (post.userId === userID) {
+        const userPost = document.createElement("div");
+        userPost.textContent += `Post Title: ${post.title}`;
+        userPost.textContent += `Post Content: ${post.body}`;
+        userDiv.appendChild(userPost);
+      }
+    });
+  } catch (error) {
+    console.log("An error occured: ", error);
+  }
+}
+
+const userIDp = 2;
+fetchPosts(userIDp);
